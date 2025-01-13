5f21acf30425948b138f8d849d77275b22c386f8
ppoitier <hanofound@hormail.com>
2025-01-13 19:31:17 +0900

    Fix issues

--- sls/losses/ce.py
+++ sls/losses/ce.py
@@ -5,7 +5,7 @@ from torch.nn.functional import cross_entropy
 class CrossEntropyLoss(nn.Module):
     def __init__(self, weights: Tensor | None = None):
         super().__init__()
-        self.register_buffer('weight', weights)
+        self.register_buffer('weights', weights)
 
     def forward(self, logits, targets):
         return cross_entropy(
