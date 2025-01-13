34c7a5499b97a069be57c9ec55a0473b3864d13e
ppoitier <hanofound@hormail.com>
2025-01-13 19:29:58 +0900

    Fix issues

--- sls/losses/focal_loss.py
+++ sls/losses/focal_loss.py
@@ -8,14 +8,12 @@ class FocalLoss(nn.Module):
         gamma: float = 2.0,
         weights: Tensor | None = None,
         reduction: str = "mean",
-        **kwargs,
     ):
         """Applies Focal Loss: https://arxiv.org/pdf/1708.02002.pdf"""
         super().__init__()
         self.gamma = gamma
         self.register_buffer("weights", weights)
         self.reduction = reduction
-        self.kwargs = kwargs
 
     def forward(self, logits: Tensor, targets: Tensor) -> Tensor:
         ce_loss = cross_entropy(
@@ -24,7 +22,6 @@ class FocalLoss(nn.Module):
             weight=self.weights,
             reduction="none",
             ignore_index=-1,
-            **self.kwargs
         )
         p_t = exp(-ce_loss)
         loss = (1 - p_t) ** self.gamma * ce_loss
