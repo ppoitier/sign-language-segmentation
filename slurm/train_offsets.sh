9a9e89cb1e5b8f12a42c1c989806ef19d3307d58
ppoitier <pierre.poitier@unamur.be>
2025-01-16 19:12:44 +0900

    Slurm file for offsets experiment

---
+++ slurm/train_offsets.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Submission script for Lucia
+#SBATCH --job-name=sls_dgs_win
+#SBATCH --time=24:00:00 # hh:mm:ss
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --gres="gpu:1"
+#SBATCH --mem-per-cpu=16384
+#SBATCH --partition=gpu
+#SBATCH --array=0-0
+#
+#SBATCH --mail-user=pierre.poitier@unamur.be
+#SBATCH --mail-type=ALL
+#
+#SBATCH --account=lsfb
+#
+#SBATCH --output=./out/offsets/dgs_%A_%a.out
+
+module purge
+module load EasyBuild/2022a
+module load Python/3.10.4-GCCcore-11.3.0
+
+# Activate Python virtual env
+source /gpfs/home/acad/unamur-fac_info/ppoitier/envs/dl/bin/activate
+
+config_files=(
+  "../config/offsets/DGS_O_MSTCN_H64_IO_OFF.yaml"
+)
+
+config_file=${config_files[$SLURM_ARRAY_TASK_ID]}
+
+nvidia-smi
+echo "Job array ID: $SLURM_ARRAY_TASK_ID"
+echo "Job start at $(date)"
+python ../main.py --config-path="$config_file"
+echo "Job end at $(date)"
