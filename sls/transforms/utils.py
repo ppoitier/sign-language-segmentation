f14f8b175a45a8660685d0b4139b5c0c64ea0958
ppoitier <hanofound@hormail.com>
2025-01-19 14:00:00 +0900

    Fix features experiment

--- sls/transforms/utils.py
+++ sls/transforms/utils.py
@@ -1,4 +1,6 @@
-from sign_language_tools.common.transforms import Compose
+from sign_language_tools.common.transforms import (
+    Compose, TransformTuple, Concatenate as ConcatenateChannels, MapTransform, Identity
+)
 from sign_language_tools.pose.transform import *
 
 
@@ -29,5 +31,12 @@ def get_transform_pipeline(pipeline_name: str):
             CenterOnLandmarks((11, 12)),
             Flatten(),
         ])
-
+    elif '||' in pipeline_name:
+        pipeline_names = pipeline_name.split('||')
+        pipelines = [get_transform_pipeline(name) for name in pipeline_names]
+        return Compose([
+            TransformTuple(Identity(), n=len(pipelines)),
+            MapTransform(pipelines),
+            ConcatenateChannels(dim=-1),
+        ])
     raise ValueError(f"Unknown transform pipeline: {pipeline_name}")
